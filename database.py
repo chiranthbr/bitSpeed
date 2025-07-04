6082c69082b8ca39c53d849e00e108d6858b6e43
Chiranth B R <chiranthgupta11@gmail.com>
2025-07-04 19:50:53 +0530

    Cleared some bugs and optimized codes

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:USZmFipoJUtkXCFdwNCarTYRhHJiouyZ@yamabiko.proxy.rlwy.net:28356/railway"
+DATABASE_URL = "database-url"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
